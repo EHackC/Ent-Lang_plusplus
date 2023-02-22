00111628095de91b8d91318f50f1476019c4108b
EHackC <lolisfun0987@gmail.com>
2023-02-23 01:04:26 +0900

    파일 이름 변경

---
+++ src/elpp.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void showHelp(){
+    cout<<"elpp"
+}
+
+int main(int argc,char* argv[]){
+    if(argc==2){
+    }else{
+        showHelp();
+    }
+    return 0;
+}
