89bf5af654eb4970ae35caa8ffbfd9dbcb52b802
EHackC <lolisfun0987@gmail.com>
2023-02-23 10:22:11 +0900

    파일 로드기능 추가

--- main.cpp
+++ main.cpp
@@ -1,34 +1,31 @@
 #include <iostream>
 #include <fstream>
-#include <windows.h>
-
-#pragma execution_character_set("utf-8")
 
 using namespace std;
 
-void showHelp(){
-    cout<<"사용법\n"<<
-          "elpp 파일이름.elpp"<<endl;
+void getFile(char* path){
+    std::ifstream ifs;
+    ifs.open(path,ios::in);
+    if (ifs.fail()){
+        cerr<<"Could not load file: "<<path<<endl;
+    }else{
+        //test
+        char line[256] = { 0, };
+        while (ifs.getline(line, 256)) {
+            std::cout << line << std::endl;
+        }
+    }
+    ifs.close();
 }
 
 int main(int argc,char* argv[]){
-    SetConsoleOutputCP(65001);
-
     if(argc==2){
-        std::ifstream ifs;
-        ifs.open(argv[1],ios::in);
-        if (ifs.fail()){
-            cerr<<"파일을 불러올수 없습니다: "<<argv[1]<<endl;
-        }else{
-            char line[256] = { 0, };
-            while (ifs.getline(line, 256)) {
-                std::cout << line << std::endl;
-            }
-        }
-        ifs.close();
+        getFile(argv[1]);
     }else{
-        showHelp();
+        char path[1024];
+        cout<<"path :";
+        cin.getline(path,1024);
+        getFile(path);
     }
-    system("pause");
     return 0;
 }
